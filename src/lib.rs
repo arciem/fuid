ad0f722787da4f5f64e968ef3d3f28d9dd316702
Wolf McNally <wolf@wolfmcnally.com>
2024-03-22 19:36:56 -0700

    Typo

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //! FUIDs are wrapped 128-bit unsigned integers, which gives them the same
 //! resolution as UUIDs. FUIDs are serialized to Base62, which is a sequence of
-//! digits and alphanumerics. This makes them shorter and easier to handle than
+//! digits and letters. This makes them shorter and easier to handle than
 //! normal UUID encoding, yet when generated randomly they use a UUID generation
 //! algorithm and are therefore isomorphic with UUIDs. One advantage of using
 //! FUIDs is that they can be converted from and to short strings that can stand
